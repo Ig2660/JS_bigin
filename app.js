bd780bead28d064d7c9524f9e426ea1e805b729a
Ig2660 <110650490+Ig2660@users.noreply.github.com>
2023-03-22 10:38:58 +0300

    Creat JS_bigin

---
+++ app.js
@@ -0,0 +1,49 @@
+//Переменные
+// let currentYear = 2023
+// console.log(++currentYear)
+// console.log(--currentYear)
+
+// if else
+// const isReady = false
+//
+// if (isReady) {
+//     console.log('Yes')
+// }else {
+//     console.log('No')
+// }
+// Тернарные выражения
+// isReady ?  console.log('Yes') :  console.log('No')
+
+// Объекты
+// const person = {
+//     firstName: 'Ig',
+//     lastName: 'Pol',
+//     year: 62,
+//     leng: ['ru', 'eng', 'de'],
+//     greet: function () {
+//         console.log('HA_HA_HA')
+//     }
+// }
+// console.log(person.year)
+// console.log(person['lastName'])
+// const key = 'leng'
+// console.log(person[key])
+// person.isProgr = true
+// person.greet()
+// console.log(person)
+
+// console.log(Math.abs(56.8))
+// console.log(Math.abs(-56.8))
+
+function getRandomBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+console.log(getRandomBetween(31, 97));
+
+
+
+
+
+
+
